0b24a27ef698c6cb0e2e29409b56076ca84f3b93
mohammedrimshan <rimshanshanu55@gmail.com>
2025-01-09 17:26:29 +0530

    some changes

--- week 2/Day 1/sorting/quickSort.js
+++ week 2/Day 1/sorting/quickSort.js
@@ -52,35 +52,6 @@ function partition(arr, low, high) {
     return i + 1; // Return the pivot index
 }
 
-function quickSort(arr, low = 0, high = arr.length - 1) {
-    if (low < high) {
-        // Partition the array and get the pivot index
-        let pivotIndex = partition(arr, low, high);
-
-        // Recursively sort elements before and after the pivot
-        quickSort(arr, low, pivotIndex - 1);
-        quickSort(arr, pivotIndex + 1, high);
-    }
-    return arr;
-}
-
-function partition(arr, low, high) {
-    // Choose the last element as the pivot
-    let pivot = arr[high];
-    let i = low - 1; // Index of smaller element
-
-    for (let j = low; j < high; j++) {
-        // If the current element is smaller than or equal to the pivot
-        if (arr[j] <= pivot) {
-            i++;
-            [arr[i], arr[j]] = [arr[j], arr[i]]; // Swap
-        }
-    }
-    // Swap the pivot with the element at i+1
-    [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]];
-    return i + 1; // Return the pivot index
-}
-
 // Example usage
 const array2 = [29, 10, 14, 37, 13];
 console.log("Original Array:", array2);
