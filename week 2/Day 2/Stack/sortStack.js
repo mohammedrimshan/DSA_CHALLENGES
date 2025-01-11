0fd9e89393e4f3de48c9400efdd4858ff90c9036
mohammedrimshan <rimshanshanu55@gmail.com>
2025-01-11 17:19:44 +0530

    some changes

--- week 2/Day 2/Stack/sortStack.js
+++ week 2/Day 2/Stack/sortStack.js
@@ -1,34 +1,46 @@
 class Stack {
   constructor() {
     this.stack = [];
-    this.top = -1;
   }
+
   push(data) {
     this.stack.push(data);
   }
+
   pop() {
-    this.stack.pop();
+    return this.stack.pop();
   }
+
   sortwithstack() {
     let tempstack = [];
     let input = this.stack;
+
     while (input.length > 0) {
       let tmp = input.pop();
       while (tempstack.length > 0 && tempstack[tempstack.length - 1] < tmp) {
-        input.push(tempstack[tempstack.length - 1]);
-        tempstack.pop();
+        input.push(tempstack.pop());
       }
       tempstack.push(tmp);
     }
-    return tempstack;
+    this.stack = tempstack;
+  }
+
+  printStack() {
+    console.log(this.stack.join(" -> "));
   }
 }
 
-const stack = new Stack(10);
+const stack = new Stack();
 stack.push(5);
 stack.push(1);
 stack.push(4);
 stack.push(2);
 stack.push(3);
-console.log(stack.sortwithstack());
-console.log(stack);
+
+console.log("Before sorting:");
+stack.printStack();
+
+stack.sortwithstack();
+
+console.log("After sorting:");
+stack.printStack();
